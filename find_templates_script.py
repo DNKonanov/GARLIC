8c08127226bd23ba3368d3ba6a30cc056ef6f9ca
DNKonanov <konanovdmitriy@gmail.com>
2019-03-15 16:57:28 +0300

    some fixes

--- find_templates_script.py
+++ find_templates_script.py
@@ -1,19 +1,19 @@
 from search.VF2_find_template import find_template
 import argparse
 from gene_graph_lib.compute_complexity import GenomeGraph
-from gene_graph_lib.pattern_searching import count_patten_in_gen_for_each, GarlicPattern, find_spyder, find_transposition
+from gene_graph_lib.pattern_searching import count_patten_in_gen_for_each, GarlicPattern, find_spyder, find_transposition, find_garlic
 from parser.graphbuilder import Graph
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--template', type=str, default=None, help='template (for example "\'1\'>\'2\'>\'3\'"]')
+parser.add_argument('--template', type=str, default="'1'>'2'", help='template (for example "\'1\'>\'2\'>\'3\'"]')
 parser.add_argument('--graph', type=str, default=None, help='sif file with graph structure')
 parser.add_argument('--type', type=str, default=None, help='optimized pattern name')
 parser.add_argument('-o', default='out.txt', type=str, help='output file')
 args = parser.parse_args()
 
 g = GenomeGraph()
-g.read_graph(args.graph)
+g.read_graph(args.graph, generate_freq=True)
 
 template = Graph.parse(args.template)
 
@@ -26,9 +26,9 @@ elif args.type == 'spider':
 elif args.type == 'penguin':
     hits = find_transposition(g)
 
-elif args.type == 'spider':
+elif args.type == 'garlic':
     garlic = GarlicPattern()
-    hits = count_patten_in_gen_for_each(g.list_graph, garlic)
+    hits = find_garlic(g)
 else:
     print('invalid pattern type')
 
--- run.sh
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "second commit"
-git push -u origin master
